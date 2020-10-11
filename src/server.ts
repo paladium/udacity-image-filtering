702a1932f00455cc05230f67eb5d7a08e41f3001
Dragma <musaevilkin29@gmail.com>
2020-10-11 11:24:26 +0400

    Add authentication using basicauth

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
+export function requireAuth(req: Request, res: Response, next: NextFunction){
+    if(!req.headers || !req.headers.authorization){
+        return res.status(401).send({message: "Authorization header required"});
+    }
+    const basicAuth = req.headers.authorization.split(" ");
+    if(basicAuth.length != 2){
+        return res.status(401).send({message: "Invalid token"});
+    }
+    const authToken = basicAuth[1];
+    //Decode the token using base64 and split using :
+    const decodedToken = Buffer.from(authToken, "base64").toString().split(":");
+    if(decodedToken.length != 2){
+        return res.status(401).send({message: "Invalid token"});
+    }
+    const [username, password] = decodedToken;
+    if(username != "test" || password != "test"){
+        return res.status(401).send({message: "Invalid credentials"});
+    }
+    return next();
+};
 
 (async () => {
 
@@ -28,7 +48,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
     /**************************************************************************** */
 
-    app.get("/filteredimage", async (req, res) => {
+    app.get("/filteredimage", requireAuth, async (req, res) => {
         const { image_url } = req.query;
         if (!image_url || image_url == "") {
             res.status(400).send("image_url is required");
